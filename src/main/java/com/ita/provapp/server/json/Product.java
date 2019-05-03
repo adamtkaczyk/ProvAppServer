430508105f355eb26591638f7c981394f6a679c3
Adam Tkaczyk <adamtkaczyk90@gmail.com>
2019-05-03 17:14:39 +0200

    Add firts drft for REST AIP product service

---
+++ src/main/java/com/ita/provapp/server/json/Product.java
@@ -0,0 +1,13 @@
+package com.ita.provapp.server.json;
+
+public class Product {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
